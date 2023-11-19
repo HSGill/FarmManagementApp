fe0f1cc3798d1abd3903a194e18f1ff85d97756f
Harry Gill <harry.gill@aderant.com>
2023-11-19 16:47:55 +1300

    Add Farm Details Page

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using FarmManagementApp;
+using FarmManagementApp.Services;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add MudBlazor services
+builder.Services.AddMudServices();
+builder.Services.AddScoped<IFarmService, FarmService>();
 
 await builder.Build().RunAsync();
